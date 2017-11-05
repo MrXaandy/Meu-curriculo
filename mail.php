65f3476031c35da1dbfc19715803e0574f74e544
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 16:46:45 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -1,18 +1,24 @@
 <?php
     require("class.phpmailer.php");
     require("class.smtp.php");
-    $mail = new PHPMailer();
-    $mail->IsSMTP();  // telling the class to use SMTP
-    $mail->Host     = "smtp.gmail.com"; // SMTP server
-    $mail->From     = "alexandresx9@gmail.com";
-    $mail->AddAddress("alexandresx9@mail.com");
-    $mail->Subject  = "First PHPMailer Message";
-    $mail->Body     = "Hi! \n\n This is my first e-mail sent through PHPMailer.";
-    $mail->WordWrap = 50;
-    if(!$mail->Send()) {
-        echo 'Message was not sent.';
-        echo 'Mailer error: ' . $mail->ErrorInfo;
-    }else {
-        echo 'Message has been sent.';
-    }
+    $mail = new PHPMailer(); // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "alexandresx9@gmail.com";
+    $mail->Password = "freestepporra";
+    $mail->SetFrom("alexandresx9@gmail.com");
+    $mail->Subject = "Test";
+    $mail->Body = "hello";
+    $mail->AddAddress("alexandresx9@gmail.com");
+    
+     if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+     } else {
+        echo "Message has been sent";
+     }
 ?>
