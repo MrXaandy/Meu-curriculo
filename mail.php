c00d051bd9dc5c5dcb2efd514dee437c7701948b
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 16:30:33 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
@@ -7,12 +7,12 @@ $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
-$mail->Username = "alexandresx9@gmail.com";
-$mail->Password = "freestepporra";
-$mail->SetFrom("alexandresx9@gmail.com");
+$mail->Username = "email@gmail.com";
+$mail->Password = "password";
+$mail->SetFrom("example@gmail.com");
 $mail->Subject = "Test";
 $mail->Body = "hello";
-$mail->AddAddress("alexandresx9@gmail.com");
+$mail->AddAddress("email@gmail.com");
 
  if(!$mail->Send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
