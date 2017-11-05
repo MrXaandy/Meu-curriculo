9bddcefde33b6007cfd0ff8e2f83d11dc3f99e6f
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 16:32:29 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -1,22 +1,12 @@
 <?php
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "email@gmail.com";
-$mail->Password = "password";
-$mail->SetFrom("example@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress("email@gmail.com");
+    ini_set("SMTP", "aspmx.l.google.com");
+    ini_set("sendmail_from", "alexandresx9@gmail.com");
 
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
- ?>
+    $message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = alexandresx9@address.com";
+
+    $headers = "From: alexandresx9@gmail.com";
+
+
+    mail("Sending@provider.com", "Testing", $message, $headers);
+    echo "Check your email now....<BR/>";
+?>
