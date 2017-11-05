457711883aad902b08cc1494cd9eecdbfac7ed61
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 17:54:50 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -1,19 +1,29 @@
 <?php
     require("class.phpmailer.php");
     require("class.smtp.php");
-    $mail = new PHPMailer(); // create a new object
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+    $nomeremetente = $_POST['nome'];
+    $emailremetente = trim($_POST['email']);
+    $assunto = $_POST['assunto'];
+    $mensagem = $_POST['mensagem'];
+    $mensagemHTML = '<strong>Formulário de Contato</strong>
+    <p><b>Nome:</b> '.$nomeremetente.'
+    <p><b>E-Mail:</b> '.$emailremetente.'
+    <p><b>Assunto:</b> '.$assunto.'
+    <p><b>Mensagem:</b> '.$mensagem.'</p>
+    <hr>';
+    $mail = new PHPMailer(); 
+    $mail->IsSMTP(); 
+    $mail->SMTPDebug = 1; 
+    $mail->SMTPAuth = true; 
+    $mail->SMTPSecure = 'tls'; 
     $mail->Host = "smtp.gmail.com";
-    $mail->Port = 587; // or 587 465
+    $mail->Port = 587; 
     $mail->IsHTML(true);
     $mail->Username = "alexandresx9@gmail.com";
     $mail->Password = "freestepporra";
     $mail->SetFrom("alexandresx9@gmail.com");
-    $mail->Subject = "Test";
-    $mail->Body = "hello";
+    $mail->Subject = $assunto;
+    $mail->Body = $mensagem;
     $mail->AddAddress("alexandresx9@gmail.com");
     
      if(!$mail->Send()) {
