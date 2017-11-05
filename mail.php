48a8edd71be2f8ab038aa0df7cc669a94dc1fc14
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 17:05:01 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -2,12 +2,12 @@
     require("class.phpmailer.php");
     require("class.smtp.php");
     $mail = new PHPMailer(); // create a new object
-    //$mail->IsSMTP(); // enable SMTP
+    $mail->IsSMTP(); // enable SMTP
     $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth = true; // authentication enabled
     $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
     $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
+    $mail->Port = 587; // or 587
     $mail->IsHTML(true);
     $mail->Username = "alexandresx9@gmail.com";
     $mail->Password = "freestepporra";
