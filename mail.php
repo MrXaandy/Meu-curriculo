668dcf16fcaf9b17c9a6cfc2ac422784f7e7e589
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 17:25:52 -0200

    Modified mail.php

--- mail.php
+++ mail.php
@@ -5,9 +5,9 @@
     $mail->IsSMTP(); // enable SMTP
     $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
     $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
+    $mail->Port = 587; // or 587 465
     $mail->IsHTML(true);
     $mail->Username = "alexandresx9@gmail.com";
     $mail->Password = "freestepporra";
