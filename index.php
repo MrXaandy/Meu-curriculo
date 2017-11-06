612adc30ddeb9b7da429e8ccba248c0a91e78d4f
Alexandre da Silva <alexandresx9@gmail.com>
2017-11-05 23:16:02 -0200

    index.php

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
         window.onscroll = function() {scrollFunction()};
         
         function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 40) {
                 document.getElementById("btnTopo").style.display = "block";
             } else {
                 document.getElementById("btnTopo").style.display = "none";
